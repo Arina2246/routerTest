b3d8b5d5d9a95b991554a3ff1e099689a0500249
Arina2246 <arinanebylova@gmail.com>
2023-05-08 22:23:43 +0200

    feat: add smth

--- router-test-for-project/src/App.js
+++ router-test-for-project/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <button ><a href={`routerTest/1`}>1</a></button>
       <button ><a href={`2`}>2</a></button>
+      <Link to="/1">About</Link>
+      <Link to="1">About</Link>
+      <Link to="routerTest/1">About</Link>
+      <Link to="/routerTest/1">About</Link>
     </div>
   );
 }
