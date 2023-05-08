2fcdb24413d945de74bcc8dcc3d6d658ba496bbc
Arina2246 <arinanebylova@gmail.com>
2023-05-08 22:26:40 +0200

    feat: add smth

--- router-test-for-project/src/App.js
+++ router-test-for-project/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <button ><a href={`routerTest/1`}>1</a></button>
       <button ><a href={`2`}>2</a></button>
-      <Link to="/1">About</Link>
       <Link to="1">About</Link>
-      <Link to="routerTest/1">About</Link>
       <Link to="/routerTest/1">About</Link>
     </div>
   );
