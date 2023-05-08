374c68ff0e691c3de8c7a93e1dc05431e7ea52a2
Arina2246 <arinanebylova@gmail.com>
2023-05-08 22:17:32 +0200

    feat: add smth

--- router-test-for-project/src/App.js
+++ router-test-for-project/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button ><a href={`/1`}>1</a></button>
+      <button ><a href={`routerTest/1`}>1</a></button>
       <button ><a href={`/2`}>2</a></button>
     </div>
   );
